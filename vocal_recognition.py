e1b2613e1c66847f19a2abc86b2309f52b8753cb
Sandra-I <sandra.ibrahim@hotmail.fr>
2021-11-01 18:09:55 +0100

    feat: speech recognition first step

---
+++ vocal_recognition.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+
+rec_vocal = sr.Recognizer()
+micro = sr.Microphone()
+
+with micro as source:
+    print("Dites quelque chose")
+    audio = rec_vocal.listen(source)
+    print("Okay")
+try:
+    text = rec_vocal.recognize_google(audio, language="fr-FR")
+    print("Vous avez dit : " + text)
+except sr.UnknownValueError:
+    print("L'audio n'as pas été compris")
+except sr.RequestError as e:
+    print("Le service Google Speech API ne fonctionne plus" + format(e))
