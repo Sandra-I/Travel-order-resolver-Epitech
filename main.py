31ab69fdd7439c12977d7b1d250ebabef2dee6b2
Sandra-I <sandra.ibrahim@hotmail.fr>
2021-11-01 18:21:32 +0100

    feat: speech recognition step function

---
+++ main.py
@@ -0,0 +1,5 @@
+import speech_recognition as sr
+from vocal_recognition import speech_rec
+micro = sr.Microphone()
+
+speech_rec(micro)
--- vocal_recognition.py
+++ vocal_recognition.py
@@ -3,14 +3,17 @@ import speech_recognition as sr
 rec_vocal = sr.Recognizer()
 micro = sr.Microphone()
 
-with micro as source:
-    print("Dites quelque chose")
-    audio = rec_vocal.listen(source)
-    print("Okay")
-try:
-    text = rec_vocal.recognize_google(audio, language="fr-FR")
-    print("Vous avez dit : " + text)
-except sr.UnknownValueError:
-    print("L'audio n'as pas été compris")
-except sr.RequestError as e:
-    print("Le service Google Speech API ne fonctionne plus" + format(e))
+
+def speech_rec(voice):
+    with voice as source:
+        print("Dites quelque chose")
+        audio = rec_vocal.listen(source)
+        print("Okay")
+
+    try:
+        text = rec_vocal.recognize_google(audio, language="fr-FR")
+        print("Vous avez dit : " + text)
+    except sr.UnknownValueError:
+        print("L'audio n'as pas été compris")
+    except sr.RequestError as e:
+        print("Le service Google Speech API ne fonctionne plus" + format(e))
