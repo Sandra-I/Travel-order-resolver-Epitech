ba341558fef1811b82e68a7020fe2b53e2355717
Sandra-I <sandra.ibrahim@hotmail.fr>
2022-01-09 21:53:09 +0100

    Feat: Mock itinerary and order them

--- front/src/components/HomePage.vue
+++ front/src/components/HomePage.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
-    <div class="row justify-content-md-center">
+    <div class="row justify-content-md-center my-5" id="recorderBloc">
+      <h1>Où souhaitez-vous aller ?</h1>
       <div class="col-12"><p>Cliquez sur le micro pour indiquer votre trajet !</p></div>
       <div class="col-12">
         <SpeechRecognition lang="fr-FR" :white="false" @end="speechEnd" class="icon"/>
       </div>
     </div>
       
-    <div class="row">
+    <div class="row my-5">
       <div class="col-12">
-        <h1 v-if="isTextReceived">Text reçu => {{ textSent }}</h1>
-        <h1 v-else>No text recording</h1>
+        <h1>Votre trajet</h1>
+        <p v-if="isTextReceived">{{ textSent }}</p>
+        <p v-else>Pas de trajet demandé</p>
+      </div>
+    </div>
+
+    <div class="row my-5">
+      <div class="col">
+        <h1>Les itinéraires</h1>
+        <div v-for="(itinerary, index) in itinerariesSort" :key="index">
+          <p>{{ itinerary.cities }}</p>
+          <p>Temps de trajet : {{ itinerary.distance }}</p>
+          <!-- <p v-for="it in itinerary" :key="it">{{ it }}</p> -->
+          <hr>
+        </div>
       </div>
     </div>
   </div>
@@ -25,7 +39,21 @@ export default Vue.extend({
   data() {
     return {
       isTextReceived: false,
-      textSent: ''
+      textSent: '',
+      // itineraries: {},
+      // itineraries: [['Paris', 'Annecy', 'Grenoble', 'Marseille'], 47],
+      itineraries: [
+        { cities: ['Paris', 'Annecy', 'Grenoble', 'Marseille'], distance: 47 },
+        { cities: ['Paris', 'Grenoble', 'Marseille'], distance: 40 },
+        { cities: ['Paris', 'Marseille'], distance: 30 }
+      ]
+    }
+  },
+  computed: {
+    itinerariesSort() {
+      return [...this.itineraries].sort(function (a: { distance: number; }, b: { distance: number; }) {
+        return a.distance - b.distance;
+      });
     }
   },
   methods: {
@@ -36,7 +64,6 @@ export default Vue.extend({
       this.sendText(textRecorded);
     },
     sendText(text: string) {
-      console.log(text)
       axios
         .post('http://127.0.0.1:5000/vocal', {text})
         .then(response => {
@@ -55,25 +82,30 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
-.icon {
+#recorderBloc {
+  background-color: rgb(255, 223, 223);
 
-  img {
-    width: 64px;
-    height: 64px;
-    -ms-transition: width 5s, height 5s, transform 5s;
-    -webkit-transition: width 5s, height 5s, transform 5s;
-    transition: width 5s, height 5s, transform 5s;
-  }
+  .icon {
+    img {
+      margin-top: 20px;
+      width: 64px;
+      height: 64px;
+      -ms-transition: width 5s, height 5s, transform 5s;
+      -webkit-transition: width 5s, height 5s, transform 5s;
+      transition: width 5s, height 5s, transform 5s;
+    }
 
-  :hover {
-    cursor: pointer;
-    width: 100px;
-    height: 100px;
-    // background-color: gold;
-    -ms-transform: scale(1.5); /* IE 9 */
-    -webkit-transform: scale(1.5); /* Safari 3-8 */
-    transform: scale(1.5);
+    :hover {
+      cursor: pointer;
+      width: 100px;
+      height: 100px;
+      // background-color: gold;
+      -ms-transform: scale(1.5); /* IE 9 */
+      -webkit-transform: scale(1.5); /* Safari 3-8 */
+      transform: scale(1.5);
+    }
   }
 }
+
 
 </style>
