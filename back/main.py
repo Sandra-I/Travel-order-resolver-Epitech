7c34ddd2852255cdc7c8e905813da60346912f0b
Sandra-I <sandra.ibrahim@hotmail.fr>
2022-01-22 21:09:19 +0100

    Chore: some tests again

--- back/main.py
+++ back/main.py
@@ -1,22 +1,22 @@
+from http.client import HTTPResponse
 import json
 from flask import Flask, jsonify, request 
 from flask_cors import CORS, cross_origin
 import source.TripEvaluation.main_eval as tp
 import source.Itinerary.itinerary as it
+import requests as rq
 
 app = Flask(__name__) 
 CORS(app, resources={r"/foo": {"origins": "*"}})
 # CORS(app, resources=r'http://localhost:8080/*')
 
-@app.route('/', methods = ['GET', 'POST']) 
+@app.route('/', methods = ['GET']) 
 @cross_origin() 
 def home():
-    print('home') 
-    if(request.method == 'POST'): 
-        data = json.loads(request.data)
-        print('data', data, type(data))
-        return jsonify({'receivedText': data['text']})
-    return jsonify({'data': "toto"})
+    print('home 2') 
+    r = rq.get('https://httpbin.org/status/418')
+    print(r.text)
+    return jsonify({'data': "Welcome!"})
 
 @app.route('/vocal', methods = ['POST'])
 @cross_origin()
