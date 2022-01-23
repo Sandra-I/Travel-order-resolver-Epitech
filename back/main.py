027643ac6ca96bd24de95e2b8e65b69c9f79f093
Sandra-I <sandra.ibrahim@hotmail.fr>
2022-01-23 16:24:04 +0100

    Chore: clean code

--- back/main.py
+++ back/main.py
@@ -1,10 +1,8 @@
-from http.client import HTTPResponse
 import json
 from flask import Flask, jsonify, request 
 from flask_cors import CORS, cross_origin
 import source.TripEvaluation.main_eval as tp
 import source.Itinerary.itinerary as it
-import requests as rq
 
 app = Flask(__name__) 
 CORS(app, resources={r"/foo": {"origins": "*"}})
@@ -13,21 +11,16 @@ CORS(app, resources={r"/foo": {"origins": "*"}})
 @app.route('/', methods = ['GET']) 
 @cross_origin() 
 def home():
-    print('home 2') 
-    r = rq.get('https://httpbin.org/status/418')
-    print(r.text)
     return jsonify({'data': "Welcome!"})
 
 @app.route('/vocal', methods = ['POST'])
 @cross_origin()
 def vocal():
-    print('vocal')
     data = json.loads(request.data)
     phrase = str(data['text'])
     print('data', phrase, type(phrase))
     if phrase:
         tripArray = tp.main_test(phrase)
-    # Ajouter une validation avant de call cette méthode
         if tripArray != 'Invalid Sentence':
             print(tripArray)
             itinerary = it.get_itinerary(locArray = tripArray)
